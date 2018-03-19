c66f89c1bfea17c97adb47cc1c7186343a7634db
Gadzhibala Pirmagomedov <gadzhi@yandex.ru>
2018-03-19 17:35:37 +0300

    Test

--- mapper.py
+++ mapper.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
+"""mapper.py"""
+
 import sys
 
+# input comes from STDIN (standard input)
 for line in sys.stdin:
+    # remove leading and trailing whitespace
     line = line.strip()
-    columns = line.split(',') # split line into parts
-    # the data is messy, only read those having correct column count
-    if len(columns) == 14:
-        try:
-            countUpVotes = int(columns[7])
-            print "%s\t%s" % (columns[8],countUpVotes)
-        except ValueError:
-            pass
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print '%s\t%s' % (word, 1)
